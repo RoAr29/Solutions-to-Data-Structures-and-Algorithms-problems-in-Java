7db47d04aeae0b0c62bdf5006fc882869751b978
RoAr29 <agrawalsp_2@rknec.edu>
2026-03-14 19:48:44 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (70.74%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,7 +1,6 @@
 class Solution {
     public double myPow(double x, int n) {
-        double result = Math.pow(x, n);
-
-        return result;
+        double pow = Math.pow(x,n);
+        return pow;
     }
 }
