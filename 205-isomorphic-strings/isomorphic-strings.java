7f5535f94648e9114b2a25d9d5d70cab2b66b4fb
RoAr29 <agrawalsp_2@rknec.edu>
2026-06-02 18:23:16 +0530

    Time: 15 ms (46.31%) | Memory: 43.5 MB (92.51%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,33 +1,24 @@
-import java.util.*;
-
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        
-        if(s.length() != t.length()){
-            return false;
-        }
-
         HashMap<Character, Character> map = new HashMap<>();
-        HashSet<Character> mapped = new HashSet<>();
+        if(s.length() != t.length()) return false;
 
-        for(int i = 0; i < s.length(); i++){
-            
-            char c1 = s.charAt(i);
-            char c2 = t.charAt(i);
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            char ch = t.charAt(i);
 
-            if(map.containsKey(c1)){
-                if(map.get(c1) != c2){
+            if(map.containsKey(c)){
+                if(map.get(c) != ch){
                     return false;
                 }
-            } else {
-                if(mapped.contains(c2)){
-                    return false;
-                }
-                map.put(c1, c2);
-                mapped.add(c2);
             }
+            else{
+                if(map.containsValue(ch)){
+                    return false;
+                }
+            }
+            map.put(c, ch);
         }
-
         return true;
     }
 }
