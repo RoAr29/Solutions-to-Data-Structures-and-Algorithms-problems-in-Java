e3ab4c4a1676bd4df3596112d6d59778f266a29f
RoAr29 <agrawalsp_2@rknec.edu>
2026-06-25 18:23:19 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (95.13%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int[] ash = new int[nums.length];
-        int i=0, j=n;
-        int index=0;
+        int i=0; 
+        int j=nums.length/2;
+        int[] num = new int[nums.length];
+        int idx = 0;
 
-        while(i<n){
-            ash[index++] = nums[i];
-            ash[index++] = nums[j];
+        while(j<nums.length){
+            num[idx++] = nums[i];
+            num[idx++] = nums[j];
             i++;
             j++;
         }
-        return ash;
+        return num;
     }
 }
