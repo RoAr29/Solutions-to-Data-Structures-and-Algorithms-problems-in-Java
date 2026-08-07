d3e8ced00d044c186e3cf328dd49c8ec489c2f3d
RoAr29 <agrawalsp_2@rknec.edu>
2026-08-07 22:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (57.51%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        int[] dp = new int[nums.length];
+
+        Arrays.fill(dp, -1);
+        
+       return solve(nums, 0, dp);
+    }
+
+    public int solve(int[] nums,int i, int[] dp){
+
+        if(i >= nums.length){
+        return 0;
+        } 
+
+       if(dp[i] != -1){
+        return dp[i];
+       }
+
+       int choice1 = nums[i] + solve(nums, i+2, dp);
+       int choice2 = solve(nums, i+1, dp);
+
+       dp[i] = Math.max(choice1, choice2);
+
+       return dp[i];
+    }
+}
