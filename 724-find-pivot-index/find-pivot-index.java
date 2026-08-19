8a70880884acf6e3c6de088eafb792dbe8b2d160
RoAr29 <agrawalsp_2@rknec.edu>
2026-08-19 17:39:04 +0530

    Time: 1 ms (97.57%) | Memory: 47.2 MB (89.17%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -4,13 +4,18 @@ class Solution {
         int total = 0;
         int left = 0, right = 0;
 
-        for(int i:nums){
+        for(int i : nums){
             total += i;
         }
 
         for(int i=0; i<nums.length; i++){
-            right = total - nums[i] - left;
-            if(right == left) return i;
+            
+            right = total - left-nums[i];
+
+            if(left == right){
+                return i;
+            }
+
             left += nums[i];
         }
         return -1;
